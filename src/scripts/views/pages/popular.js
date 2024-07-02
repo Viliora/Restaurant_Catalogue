6584df31d8e6c04a14f24a71b4806fd70ffb5d15
Viliora <144647170+Viliora@users.noreply.github.com>
2024-07-02 22:42:30 +0700

    add src
    menambahkan folder src pada project

---
+++ src/scripts/views/pages/popular.js
@@ -0,0 +1,24 @@
+/* eslint-disable no-shadow */
+import TheRestaurantDbSource from '../../data/therestaurantdb-source';
+import { createRestaurantItemTemplate } from '../templates/template-creator';
+
+const Popular = {
+  async render() {
+    return `
+      <div class="content">
+        <h2 class="content__heading">Popular Restaurants</h2>
+        <div id="restaurants" class="restaurants"></div>
+      </div>
+    `;
+  },
+
+  async afterRender() {
+    const restaurants = await TheRestaurantDbSource.popularRestaurant();
+    const restaurantsContainer = document.querySelector('#restaurants');
+    restaurants.forEach((restaurant) => {
+      restaurantsContainer.innerHTML += createRestaurantItemTemplate(restaurant);
+    });
+  },
+};
+
+export default Popular;
